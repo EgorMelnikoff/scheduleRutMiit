f5de525c7931222ac14f73c101d170dbda4b9188
egor <goodperson2033@gmail.com>
2025-06-30 19:24:21 +0300

    remove import shadow

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/schedule/ScheduleEvent.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/schedule/ScheduleEvent.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
