fe0a9376e48c033033c0920abe20296d8eba1687
egor <goodperson2033@gmail.com>
2025-07-08 01:46:11 +0300

    Add new NavBar

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/Main.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/Main.kt
@@ -1,19 +1,32 @@
 package com.egormelnikoff.schedulerutmiit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -26,11 +39,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.egormelnikoff.schedulerutmiit.data.Event
@@ -61,22 +80,22 @@ fun Main(
     val barItems = arrayOf(
         BarItem(
             title = LocalContext.current.getString(R.string.search),
-            image = ImageVector.vectorResource(R.drawable.search),
+            icon = ImageVector.vectorResource(R.drawable.search),
             route = Routes.Search.route
         ),
         BarItem(
             title = LocalContext.current.getString(R.string.schedule),
-            image = ImageVector.vectorResource(R.drawable.schedule),
+            icon = ImageVector.vectorResource(R.drawable.schedule),
             route = Routes.Schedule.route
         ),
         BarItem(
             title = LocalContext.current.getString(R.string.news),
-            image = ImageVector.vectorResource(R.drawable.news),
+            icon = ImageVector.vectorResource(R.drawable.news),
             route = Routes.News.route
         ),
         BarItem(
             title = LocalContext.current.getString(R.string.settings),
-            image = ImageVector.vectorResource(R.drawable.settings),
+            icon = ImageVector.vectorResource(R.drawable.settings),
             route = Routes.Settings.route
         )
     )
@@ -173,7 +192,7 @@ fun Main(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
             NavigationBar(
-                containerColor = MaterialTheme.colorScheme.background
+                containerColor = Color.Transparent
             ) {
                 val navigationBarItemColors = NavigationBarItemColors(
                     selectedIconColor = MaterialTheme.colorScheme.primary,
@@ -184,60 +203,119 @@ fun Main(
                     disabledIconColor = Color.Unspecified,
                     disabledTextColor = Color.Unspecified
                 )
-                barItems.forEach { barItem ->
-                    NavigationBarItem(
-                        icon = {
-                            Icon(
-                                imageVector = barItem.image,
-                                contentDescription = barItem.title
-                            )
-                        },
-                        label = {
-                            Text(
-                                text = barItem.title,
-                                fontSize = 10.sp
-                            )
-                        },
-                        selected = currentRoute == barItem.route,
-                        colors = navigationBarItemColors,
-                        onClick = {
-                            if (currentRoute == barItem.route) {
-                                when (barItem.route) {
-                                    Routes.Schedule.route -> {
-                                        showDialogEvent = false
-                                    }
-                                    Routes.Settings.route -> {
-                                        showDialogInfo = false
-                                        //showDialogAddSchedule = false
-                                        showDialogSchedules = false
-                                    }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(
+                        16.dp,
+                        Alignment.CenterHorizontally
+                    )
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .height(56.dp)
+                            .border(0.1.dp, MaterialTheme.colorScheme.outline, CircleShape)
+                            .clip(CircleShape)
+                            .background(
+                                MaterialTheme.colorScheme.background.copy(
+                                    alpha = 0.85f
+                                )
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        barItems.forEach { barItem ->
+                            NavigationBarItemCustom(
+                                icon = barItem.icon,
+                                title = barItem.title,
+                                colors = navigationBarItemColors,
+                                isSelected = currentRoute == barItem.route,
+                                onClick = {
+                                    if (currentRoute == barItem.route) {
+                                        when (barItem.route) {
+                                            Routes.Schedule.route -> {
+                                                showDialogEvent = false
+                                            }
 
-                                    Routes.Search.route -> {
-                                        query = ""
-                                        searchViewModel.clearData()
-                                    }
+                                            Routes.Settings.route -> {
+                                                showDialogInfo = false
+                                                //showDialogAddSchedule = false
+                                                showDialogSchedules = false
+                                            }
 
-                                    Routes.News.route -> {
-                                        showDialogNews = false
+                                            Routes.Search.route -> {
+                                                query = ""
+                                                searchViewModel.clearData()
+                                            }
+
+                                            Routes.News.route -> {
+                                                showDialogNews = false
+                                            }
+                                        }
+                                    } else {
+                                        currentRoute = barItem.route
                                     }
                                 }
-                            } else {
-                                currentRoute = barItem.route
-                            }
+                            )
                         }
-                    )
+                    }
+//                    Button(
+//                        modifier = Modifier
+//                            .height(56.dp)
+//                            .width(56.dp),
+//                        onClick = {},
+//                        shape = CircleShape,
+//                        colors = ButtonDefaults.buttonColors().copy(
+//                            containerColor = MaterialTheme.colorScheme.primary,
+//                            contentColor = MaterialTheme.colorScheme.onPrimary
+//                        ),
+//                        contentPadding = PaddingValues(0.dp)
+//                    ) {
+//                        Icon(
+//                            imageVector = ImageVector.vectorResource(R.drawable.add),
+//                            contentDescription = null
+//                        )
+//                    }
                 }
             }
         },
         snackbarHost = {
             SnackbarHost(snackbarHostState) { data ->
-                Snackbar(
-                    modifier = Modifier.padding(horizontal = 8.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    containerColor = MaterialTheme.colorScheme.surface,
-                    contentColor = MaterialTheme.colorScheme.onBackground
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .background(
+                            MaterialTheme.colorScheme.error,
+                            RoundedCornerShape(12.dp)
+                        )
+                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    Text(text = data.visuals.message)
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        fontSize = 12.sp,
+                        style = TextStyle(
+                            platformStyle = PlatformTextStyle(
+                                includeFontPadding = false
+                            )
+                        ),
+                        text = data.visuals.message,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                    IconButton(
+                        onClick = {
+                            data.dismiss()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.clear),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
                 }
             }
         }
@@ -318,5 +396,46 @@ fun Main(
                 }
             }
         }
+    }
+}
+
+@Composable
+fun NavigationBarItemCustom(
+    icon: ImageVector,
+    title: String,
+    colors: NavigationBarItemColors,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .clip(CircleShape)
+            .clickable(
+                onClick = onClick
+            )
+            .padding(8.dp)
+            .width(52.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(
+            modifier = Modifier.size(24.dp),
+            imageVector = icon,
+            contentDescription = title,
+            tint = if (isSelected) colors.selectedIconColor else colors.unselectedIconColor
+        )
+        Text(
+            text = title,
+            fontSize = 8.sp,
+            fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+            style = TextStyle(
+                platformStyle = PlatformTextStyle(
+                    includeFontPadding = false
+                )
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = if (isSelected) colors.selectedTextColor else colors.unselectedTextColor
+        )
     }
 }
