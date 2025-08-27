250a3fc4c5b996d046cf49d4ff676600eb89d093
egor <goodperson2033@gmail.com>
2025-08-27 15:48:46 +0300

    change versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.egormelnikoff.schedulerutmiit"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
