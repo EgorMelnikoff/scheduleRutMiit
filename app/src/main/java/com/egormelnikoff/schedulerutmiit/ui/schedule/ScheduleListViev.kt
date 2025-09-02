dd7ed951291f5941438bc4bdf078ba61a8b4d46e
egor <goodperson2033@gmail.com>
2025-09-02 23:41:09 +0300

    Added a button to scroll up

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/schedule/ScheduleListViev.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/schedule/ScheduleListViev.kt
@@ -1,7 +1,11 @@
 package com.egormelnikoff.schedulerutmiit.ui.schedule
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,11 +15,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,6 +39,7 @@ import androidx.compose.ui.unit.sp
 import com.egormelnikoff.schedulerutmiit.R
 import com.egormelnikoff.schedulerutmiit.data.entity.Event
 import com.egormelnikoff.schedulerutmiit.data.entity.EventExtraData
+import com.egormelnikoff.schedulerutmiit.data.entity.ScheduleEntity
 import com.egormelnikoff.schedulerutmiit.ui.composable.Empty
 import kotlinx.coroutines.launch
 import java.time.LocalDate
@@ -39,59 +47,92 @@ import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.Locale
 
+@SuppressLint("FrequentlyChangingValue")
 @Composable
 fun ScheduleListView(
     onShowDialogEvent: (Pair<Event, EventExtraData?>) -> Unit,
-    scheduleListState: LazyListState,
-    isShortEvent: Boolean,
+    scheduleEntity: ScheduleEntity,
     eventsForList: List<Pair<LocalDate, List<Event>>>,
     eventsExtraData: List<EventExtraData>,
+    scheduleListState: LazyListState,
+    isShortEvent: Boolean,
     paddingBottom: Dp
 ) {
     if (eventsForList.isNotEmpty()) {
         val scope = rememberCoroutineScope()
-        LazyColumn(
-            state = scheduleListState,
-            contentPadding = PaddingValues(bottom = paddingBottom),
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            val lastIndex = eventsForList.lastIndex
-            val formatter = DateTimeFormatter.ofPattern("d MMMM")
-            eventsForList.forEachIndexed { index, events ->
-                stickyHeader {
-                    DateHeader(
-                        date = events.first,
-                        formatter = formatter,
-                        onClick = {
-                            scope.launch {
-                                scheduleListState.animateScrollToItem(0)
-                            }
-                        }
-                    )
-                }
-                val eventsForDayGrouped = events.second
-                    .sortedBy { event -> event.startDatetime!!.toLocalTime() }
-                    .groupBy { event ->
-                        event.startDatetime.toString()
-                    }
-                    .toList()
-                items(eventsForDayGrouped) { eventsGrouped ->
-                    Box (
-                        modifier = Modifier.padding(horizontal = 16.dp)
-                    ){
-                        Event(
-                            isShortEvent = isShortEvent,
-                            eventsExtraData = eventsExtraData,
-                            events = eventsGrouped.second,
-                            onShowDialogEvent = onShowDialogEvent,
+        Box {
+            LazyColumn(
+                state = scheduleListState,
+                contentPadding = PaddingValues(bottom = paddingBottom),
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                val lastIndex = eventsForList.lastIndex
+                val formatter = DateTimeFormatter.ofPattern("d MMMM")
+                eventsForList.forEachIndexed { index, events ->
+                    stickyHeader {
+                        DateHeader(
+                            currentWeek = if (scheduleEntity.recurrence != null) {
+                                calculateCurrentWeek(
+                                    date = events.first,
+                                    startDate = scheduleEntity.startDate,
+                                    firstPeriodNumber = scheduleEntity.recurrence.firstWeekNumber,
+                                    interval = scheduleEntity.recurrence.interval!!
+                                )
+                            } else null,
+                            date = events.first,
+                            formatter = formatter
                         )
                     }
-                    if (index != lastIndex) {
-                        Spacer(modifier = Modifier.height(12.dp))
+                    val eventsForDayGrouped = events.second
+                        .sortedBy { event -> event.startDatetime!!.toLocalTime() }
+                        .groupBy { event ->
+                            event.startDatetime.toString()
+                        }
+                        .toList()
+                    items(eventsForDayGrouped) { eventsGrouped ->
+                        Box(
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        ) {
+                            Event(
+                                isShortEvent = isShortEvent,
+                                eventsExtraData = eventsExtraData,
+                                events = eventsGrouped.second,
+                                onShowDialogEvent = onShowDialogEvent,
+                            )
+                        }
+                        if (index != lastIndex) {
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
                     }
                 }
             }
+            AnimatedVisibility(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = paddingBottom),
+                visible = scheduleListState.firstVisibleItemIndex != 0,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                IconButton(
+                    colors = IconButtonDefaults.iconButtonColors().copy(
+                        containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f),
+                        contentColor = MaterialTheme.colorScheme.onPrimary
+                    ),
+                    onClick = {
+                        scope.launch {
+                            scheduleListState.animateScrollToItem(0)
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.top),
+                        contentDescription = null
+                    )
+                }
+            }
         }
+
     } else {
         Empty(
             title = "¯\\_(ツ)_/¯",
@@ -104,24 +145,24 @@ fun ScheduleListView(
 
 @Composable
 fun DateHeader(
+    currentWeek: Int?,
     date: LocalDate,
-    formatter: DateTimeFormatter,
-    onClick: () -> Unit
+    formatter: DateTimeFormatter
 ) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onClick() }
             .background(MaterialTheme.colorScheme.background)
             .padding(horizontal = 16.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+        val color = MaterialTheme.colorScheme.primary
         Icon(
             modifier = Modifier.width(16.dp),
             imageVector = ImageVector.vectorResource(R.drawable.calendar),
             contentDescription = null,
-            tint = MaterialTheme.colorScheme.primary
+            tint = color
         )
         Text(
             text = "${
@@ -132,7 +173,24 @@ fun DateHeader(
             }, ${formatter.format(date)}",
             fontWeight = FontWeight.Bold,
             fontSize = 16.sp,
-            color = MaterialTheme.colorScheme.primary
+            color = color
         )
+        if (currentWeek != null) {
+            Canvas(
+                modifier = Modifier
+                    .size(3.dp)
+
+            ) {
+                drawCircle(
+                    color = color,
+                    center = center
+                )
+            }
+            Text(
+                text = "${LocalContext.current.getString(R.string.week)} $currentWeek",
+                fontSize = 16.sp,
+                color = color
+            )
+        }
     }
 }
