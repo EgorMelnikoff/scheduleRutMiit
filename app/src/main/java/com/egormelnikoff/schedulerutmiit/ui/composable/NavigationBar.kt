8649148be390754a742148e4b16020cb6fde9979
egor <goodperson2033@gmail.com>
2025-09-02 23:37:30 +0300

    New individual features

---
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/composable/NavigationBar.kt
@@ -0,0 +1,183 @@
+package com.egormelnikoff.schedulerutmiit.ui.composable
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.egormelnikoff.schedulerutmiit.ui.AppBackStack
+import com.egormelnikoff.schedulerutmiit.ui.BarItem
+import com.egormelnikoff.schedulerutmiit.ui.Routes
+
+@Composable
+fun CustomNavigationBar(
+    appBackStack: AppBackStack<Routes.Schedule>,
+    visible: Boolean,
+    barItems: Array<BarItem>
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        MaterialTheme.colorScheme.background
+                    ),
+                    startY = 0f,
+                    endY = Float.POSITIVE_INFINITY
+                )
+            )
+            .padding(horizontal = 24.dp)
+            .padding(
+                top = 12.dp,
+                bottom = WindowInsets.navigationBars.asPaddingValues()
+                    .calculateBottomPadding() + 8.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(
+            16.dp,
+            Alignment.CenterHorizontally
+        )
+    ) {
+        CustomAnimatedVisibility(
+            visible = visible
+        ) {
+            Row(
+                modifier = Modifier
+                    .height(56.dp)
+                    .border(
+                        0.1.dp,
+                        MaterialTheme.colorScheme.outline,
+                        RoundedCornerShape(16.dp)
+                    )
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(
+                        MaterialTheme.colorScheme.background
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                barItems.forEach { barItem ->
+                    CustomNavigationBarItem(
+                        barItem = barItem,
+                        isSelected = appBackStack.last() == barItem.route,
+                        onClick = {
+                            appBackStack.navigateToPage(barItem.route)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomNavigationBarItem(
+    barItem: BarItem,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.9f else 1.0f,
+        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+    )
+
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(16.dp))
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
+            )
+            .scale(scale)
+            .padding(8.dp)
+            .width(52.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(
+            modifier = Modifier.size(24.dp),
+            imageVector = if (isSelected) barItem.selectedIcon else barItem.icon,
+            contentDescription = barItem.title,
+            tint = if (isSelected) MaterialTheme.colorScheme.primary
+            else MaterialTheme.colorScheme.onSurface
+        )
+        Text(
+            text = barItem.title,
+            fontSize = 8.sp,
+            fontWeight = if (isSelected) FontWeight.Bold
+            else FontWeight.Normal,
+            style = TextStyle(
+                platformStyle = PlatformTextStyle(
+                    includeFontPadding = false
+                )
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = if (isSelected) MaterialTheme.colorScheme.primary
+            else MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
+
+@Composable
+fun CustomAnimatedVisibility(
+    visible: Boolean,
+    content: @Composable (() -> Unit),
+) {
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn() + slideInVertically(
+            initialOffsetY = { fullHeight -> fullHeight }
+        ),
+        exit = fadeOut() + slideOutVertically(
+            targetOffsetY = { fullHeight -> fullHeight }
+        )
+    ) {
+        content.invoke()
+    }
+}
---
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/composable/SnackbarHost.kt
@@ -0,0 +1,67 @@
+package com.egormelnikoff.schedulerutmiit.ui.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.egormelnikoff.schedulerutmiit.R
+
+@Composable
+fun CustomSnackbarHost(
+    snackBarHostState: SnackbarHostState
+) {
+    SnackbarHost(
+        modifier = Modifier.padding(horizontal = 24.dp),
+        hostState = snackBarHostState
+    ) { data ->
+        Row(
+            modifier = Modifier
+                .background(
+                    MaterialTheme.colorScheme.error,
+                    RoundedCornerShape(12.dp)
+                )
+                .padding(horizontal = 8.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                fontSize = 12.sp,
+                style = TextStyle(
+                    platformStyle = PlatformTextStyle(
+                        includeFontPadding = false
+                    )
+                ),
+                text = data.visuals.message,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            IconButton(
+                onClick = {
+                    data.dismiss()
+                }
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.clear),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+    }
+}
