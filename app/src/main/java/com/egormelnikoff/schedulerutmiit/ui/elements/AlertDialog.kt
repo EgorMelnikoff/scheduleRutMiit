ba5055814dab90b81726eae1dfb41f72d6e2477b
egor <goodperson2033@gmail.com>
2025-09-28 00:46:37 +0300

    New AlertDialog

---
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/AlertDialog.kt
@@ -0,0 +1,58 @@
+package com.egormelnikoff.schedulerutmiit.ui.elements
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.egormelnikoff.schedulerutmiit.R
+
+@Composable
+fun CustomAlertDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    confirmText: String? = null,
+    dismissText: String? = null
+) {
+    AlertDialog(
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text(
+                    text = confirmText ?: LocalContext.current.getString(R.string.yes),
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text(
+                    text = dismissText ?: LocalContext.current.getString(R.string.no),
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        },
+        containerColor = MaterialTheme.colorScheme.background,
+        titleContentColor = MaterialTheme.colorScheme.onBackground,
+        textContentColor = MaterialTheme.colorScheme.onSurface,
+    )
+}
