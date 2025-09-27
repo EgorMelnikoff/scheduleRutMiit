ff8d6ae1041924e431f9926493b28c8059165158
egor <goodperson2033@gmail.com>
2025-09-28 00:47:13 +0300

    Added colors

---
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/Switch.kt
@@ -0,0 +1,33 @@
+package com.egormelnikoff.schedulerutmiit.ui.elements
+
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomSwitch (
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    colors: SwitchColors? = null
+) {
+    CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 0.dp) {
+        Switch(
+            checked = checked,
+            onCheckedChange = {
+                onCheckedChange(it)
+            },
+            colors = colors ?: SwitchDefaults.colors().copy(
+                checkedTrackColor = MaterialTheme.colorScheme.primary,
+                checkedThumbColor = MaterialTheme.colorScheme.onPrimary,
+                uncheckedBorderColor = MaterialTheme.colorScheme.onSurface,
+                uncheckedThumbColor = MaterialTheme.colorScheme.onSurface,
+                uncheckedTrackColor = MaterialTheme.colorScheme.surface
+            )
+        )
+    }
+}
