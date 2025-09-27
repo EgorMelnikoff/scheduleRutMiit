44f078742981d18f00357982b855996ba4e45ed2
egor <goodperson2033@gmail.com>
2025-09-28 00:48:11 +0300

    New TimePicker and DatePicker

---
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/DatePicker.kt
@@ -0,0 +1,129 @@
+package com.egormelnikoff.schedulerutmiit.ui.elements
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.egormelnikoff.schedulerutmiit.R
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZoneOffset
+
+@OptIn(ExperimentalMaterial3Api::class)
+class DateRangeSelectableDates(
+    private val startMillis: Long,
+    private val endMillis: Long
+) : SelectableDates {
+    override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+        return utcTimeMillis in startMillis..endMillis
+    }
+
+    override fun isSelectableYear(year: Int): Boolean {
+        val yearStart =
+            LocalDate.of(year, 1, 1).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+        val yearEnd =
+            LocalDate.of(year, 12, 31).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+
+        return startMillis <= yearEnd && endMillis >= yearStart
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetDatePicker(
+    selectedDate: LocalDate?,
+    onDateSelect: (LocalDate) -> Unit,
+    onShowDialog: (Boolean) -> Unit,
+    startDate: LocalDate? = null,
+    endDate: LocalDate? = null
+) {
+    val today = LocalDate.now()
+    val minDate = startDate ?: today
+    val maxDate = endDate ?: today.plusYears(1)
+    val minMillis = minDate.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+    val maxMillis = maxDate.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+
+
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = selectedDate
+            ?.plusDays(1)
+            ?.atStartOfDay(ZoneId.systemDefault())
+            ?.toInstant()
+            ?.toEpochMilli(),
+        initialDisplayMode = DisplayMode.Picker,
+        selectableDates = DateRangeSelectableDates(minMillis, maxMillis)
+    )
+
+    ModalBottomSheet(
+        sheetState = sheetState,
+        containerColor = MaterialTheme.colorScheme.background,
+        onDismissRequest = {
+            onShowDialog(false)
+        },
+        dragHandle = {
+            BottomSheetDefaults.DragHandle(
+                color = MaterialTheme.colorScheme.outline
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState()),
+        ) {
+            DatePicker(
+                state = datePickerState,
+                showModeToggle = false,
+                colors = DatePickerDefaults.colors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                    dividerColor = MaterialTheme.colorScheme.outline,
+                    dateTextFieldColors = TextFieldDefaults.colors(
+                        focusedContainerColor = MaterialTheme.colorScheme.background,
+                        unfocusedContainerColor = MaterialTheme.colorScheme.background,
+
+                        focusedTextColor = MaterialTheme.colorScheme.onBackground,
+                        unfocusedTextColor = MaterialTheme.colorScheme.onSurface,
+
+                        focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                        unfocusedIndicatorColor = MaterialTheme.colorScheme.outline,
+
+                        focusedLabelColor = MaterialTheme.colorScheme.primary,
+                        unfocusedLabelColor = MaterialTheme.colorScheme.outline,
+
+                        errorLabelColor = MaterialTheme.colorScheme.error,
+                        errorContainerColor = MaterialTheme.colorScheme.background,
+                        errorIndicatorColor = MaterialTheme.colorScheme.error
+                    )
+                )
+            )
+            CustomButton(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                enabled = datePickerState.selectedDateMillis != null,
+                title = LocalContext.current.getString(R.string.confirm),
+                onClick = {
+                    val instant = Instant.ofEpochMilli(datePickerState.selectedDateMillis!!)
+                    onDateSelect(instant.atZone(ZoneId.systemDefault()).toLocalDate())
+                    onShowDialog(false)
+                }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/TimePicker.kt
@@ -0,0 +1,76 @@
+package com.egormelnikoff.schedulerutmiit.ui.elements
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.egormelnikoff.schedulerutmiit.R
+import java.time.LocalTime
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetTimePicker(
+    onTimeSelect: (LocalTime) -> Unit,
+    onShowDialog: (Boolean) -> Unit
+) {
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val timePickerState = rememberTimePickerState(
+        is24Hour = true
+    )
+
+    ModalBottomSheet(
+        sheetState = sheetState,
+        containerColor = MaterialTheme.colorScheme.background,
+        onDismissRequest = {
+            onShowDialog(false)
+        },
+        dragHandle = {
+            BottomSheetDefaults.DragHandle(
+                color = MaterialTheme.colorScheme.outline
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TimePicker(
+                state = timePickerState,
+                colors = TimePickerDefaults.colors(
+                    clockDialColor = MaterialTheme.colorScheme.surface,
+                    clockDialSelectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                    clockDialUnselectedContentColor = MaterialTheme.colorScheme.onSurface,
+                    selectorColor = MaterialTheme.colorScheme.primary,
+                    containerColor = MaterialTheme.colorScheme.background,
+
+                    timeSelectorSelectedContainerColor = MaterialTheme.colorScheme.primary,
+                    timeSelectorSelectedContentColor = MaterialTheme.colorScheme.onPrimary,
+
+                    timeSelectorUnselectedContainerColor = MaterialTheme.colorScheme.surface,
+                    timeSelectorUnselectedContentColor = MaterialTheme.colorScheme.onSurface
+                )
+            )
+
+            CustomButton(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                title = LocalContext.current.getString(R.string.confirm),
+                onClick = {
+                    onTimeSelect(LocalTime.of(timePickerState.hour, timePickerState.minute))
+                    onShowDialog(false)
+                }
+            )
+        }
+    }
+}
