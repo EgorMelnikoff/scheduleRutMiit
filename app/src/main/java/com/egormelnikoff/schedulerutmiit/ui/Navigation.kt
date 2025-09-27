252a2e583080e70c59307aff6f4e6c5b7070aa32
egor <goodperson2033@gmail.com>
2025-09-28 00:53:17 +0300

    Added AddSchedule, AddEvent and lastPage fun

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/Navigation.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/Navigation.kt
@@ -3,21 +3,23 @@ package com.egormelnikoff.schedulerutmiit.ui
 import androidx.compose.runtime.mutableStateListOf
 import com.egormelnikoff.schedulerutmiit.data.entity.Event
 import com.egormelnikoff.schedulerutmiit.data.entity.EventExtraData
+import com.egormelnikoff.schedulerutmiit.data.entity.ScheduleEntity
 
 sealed class Routes(val isDialog: Boolean) {
     data object Search : Routes(false)
     data object Schedule : Routes(false)
     data object NewsList : Routes(false)
     data object Settings : Routes(false)
-
     data class EventDialog(
         val event: Event,
         val eventExtraData: EventExtraData?
     ) : Routes(true)
-
     data object News : Routes(true)
-    data object Schedules : Routes(true)
     data object Info : Routes(true)
+    data object AddSchedule : Routes(true)
+    data class AddEvent(
+       val scheduleEntity: ScheduleEntity
+    ) : Routes(true)
 }
 
 class AppBackStack<T : Routes>(
@@ -25,15 +27,17 @@ class AppBackStack<T : Routes>(
 ) {
     val backStack = mutableStateListOf<Routes>(startRoute)
 
-    fun last() = backStack.last()
+    fun last() = backStack.last ()
+
+    fun lastPage() = backStack.last { !it.isDialog }
 
     fun onBack() {
         backStack.removeAt(backStack.lastIndex)
     }
 
     fun navigateToPage(route: Routes) {
-        if (backStack.last().isDialog) {
-            backStack.removeAt(backStack.lastIndex)
+        if (last().isDialog) {
+            onBack()
         }
         backStack.removeAt(0)
         backStack.add(route)
