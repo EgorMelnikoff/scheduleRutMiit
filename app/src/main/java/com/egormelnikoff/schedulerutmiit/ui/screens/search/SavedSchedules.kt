f616579c9c8c26aecedd59a62f99f9d24429d975
egor <goodperson2033@gmail.com>
2025-09-28 01:31:21 +0300

    Added vertical scroll

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/screens/search/SavedSchedules.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/screens/search/SavedSchedules.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -52,7 +54,7 @@ fun SavedSchedules(
     navigateToSchedule: () -> Unit
 ) {
     Box(
-        modifier = Modifier.padding(horizontal = 16.dp)
+        modifier = Modifier.padding(horizontal = 16.dp).verticalScroll(rememberScrollState())
     ) {
         GroupItem(
             title = LocalContext.current.getString(R.string.saved_schedules),
