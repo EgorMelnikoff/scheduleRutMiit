51ae13d13580eee60c54244508713c4a85aa9d35
egor <goodperson2033@gmail.com>
2025-10-02 22:43:35 +0300

    Replaced ModalBottomSheet to standart CustomModalBottomSheet

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/TimePicker.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/TimePicker.kt
@@ -1,12 +1,10 @@
 package com.egormelnikoff.schedulerutmiit.ui.elements
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -32,48 +30,38 @@ fun BottomSheetTimePicker(
         initialMinute = selectedTime?.minute ?: 0,
         is24Hour = true
     )
-
-    ModalBottomSheet(
+    CustomModalBottomSheet(
+        modifier = Modifier.verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
         sheetState = sheetState,
-        containerColor = MaterialTheme.colorScheme.background,
-        onDismissRequest = {
+        onDismiss = {
             onShowDialog(false)
-        },
-        dragHandle = {
-            BottomSheetDefaults.DragHandle(
-                color = MaterialTheme.colorScheme.outline
-            )
         }
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TimePicker(
-                state = timePickerState,
-                colors = TimePickerDefaults.colors(
-                    clockDialColor = MaterialTheme.colorScheme.surface,
-                    clockDialSelectedContentColor = MaterialTheme.colorScheme.onPrimary,
-                    clockDialUnselectedContentColor = MaterialTheme.colorScheme.onSurface,
-                    selectorColor = MaterialTheme.colorScheme.primary,
-                    containerColor = MaterialTheme.colorScheme.background,
+        TimePicker(
+            state = timePickerState,
+            colors = TimePickerDefaults.colors(
+                clockDialColor = MaterialTheme.colorScheme.surface,
+                clockDialSelectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                clockDialUnselectedContentColor = MaterialTheme.colorScheme.onSurface,
+                selectorColor = MaterialTheme.colorScheme.primary,
+                containerColor = MaterialTheme.colorScheme.background,
 
-                    timeSelectorSelectedContainerColor = MaterialTheme.colorScheme.primary,
-                    timeSelectorSelectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                timeSelectorSelectedContainerColor = MaterialTheme.colorScheme.primary,
+                timeSelectorSelectedContentColor = MaterialTheme.colorScheme.onPrimary,
 
-                    timeSelectorUnselectedContainerColor = MaterialTheme.colorScheme.surface,
-                    timeSelectorUnselectedContentColor = MaterialTheme.colorScheme.onSurface
-                )
+                timeSelectorUnselectedContainerColor = MaterialTheme.colorScheme.surface,
+                timeSelectorUnselectedContentColor = MaterialTheme.colorScheme.onSurface
             )
+        )
 
-            CustomButton(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                title = LocalContext.current.getString(R.string.confirm),
-                onClick = {
-                    onTimeSelect(LocalTime.of(timePickerState.hour, timePickerState.minute))
-                    onShowDialog(false)
-                }
-            )
-        }
+        CustomButton(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            buttonTitle = LocalContext.current.getString(R.string.confirm),
+            onClick = {
+                onTimeSelect(LocalTime.of(timePickerState.hour, timePickerState.minute))
+                onShowDialog(false)
+            }
+        )
     }
 }
