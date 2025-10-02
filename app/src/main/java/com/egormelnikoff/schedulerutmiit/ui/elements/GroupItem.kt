ee756349bd45616f6a09dac46b9d3497aa77408f
egor <goodperson2033@gmail.com>
2025-10-02 22:48:50 +0300

    Created universal ClickableItem()

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/GroupItem.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/GroupItem.kt
@@ -1,23 +1,44 @@
 package com.egormelnikoff.schedulerutmiit.ui.elements
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import com.egormelnikoff.schedulerutmiit.R
 
 @Composable
 fun GridGroup(
@@ -33,6 +54,7 @@ fun GridGroup(
     ) {
         if (title != null) {
             Text(
+                modifier = Modifier.padding(horizontal = 4.dp),
                 text = title,
                 fontSize = 12.sp,
                 fontWeight = FontWeight.Bold,
@@ -89,6 +111,7 @@ fun GridGroup(
 fun ColumnGroup(
     title: String? = null,
     titleColor: Color? = null,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
     edgeCorner: Dp = 16.dp,
     interiorCorner: Dp = 4.dp,
     items: List<@Composable () -> Unit>
@@ -99,6 +122,7 @@ fun ColumnGroup(
     ) {
         if (title != null) {
             Text(
+                modifier = Modifier.padding(horizontal = 4.dp),
                 text = title,
                 fontSize = 12.sp,
                 fontWeight = FontWeight.Bold,
@@ -132,8 +156,7 @@ fun ColumnGroup(
                     modifier = Modifier
                         .fillMaxWidth()
                         .clip(shape)
-                        .background(MaterialTheme.colorScheme.surface)
-
+                        .background(backgroundColor)
                 ) {
                     itemData()
                 }
@@ -156,6 +179,7 @@ fun RowGroup(
     ) {
         if (title != null) {
             Text(
+                modifier = Modifier.padding(horizontal = 4.dp),
                 text = title,
                 fontSize = 12.sp,
                 fontWeight = FontWeight.Bold,
@@ -171,8 +195,8 @@ fun RowGroup(
                 val isLast = index == items.lastIndex
 
                 val topStartRadius = if (isFirst) edgeCorner else interiorCorner
-                val topEndRadius = if (isFirst) edgeCorner else interiorCorner
-                val bottomStartRadius = if (isLast) edgeCorner else interiorCorner
+                val bottomStartRadius = if (isFirst) edgeCorner else interiorCorner
+                val topEndRadius = if (isLast) edgeCorner else interiorCorner
                 val bottomEndRadius = if (isLast) edgeCorner else interiorCorner
 
                 val shape = remember(topStartRadius, topEndRadius, bottomStartRadius, bottomEndRadius) {
@@ -194,6 +218,126 @@ fun RowGroup(
                     itemData()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ClickableItem(
+    onClick: (() -> Unit)? = null,
+    padding: Dp = 12.dp,
+    title: String,
+    subtitle: String? = null,
+    subtitleMaxLines: Int = 1,
+    imageSize: Dp = 36.dp,
+    imageUrl: String? = null,
+    imageUrlErrorTextSize: Int = 12,
+    imageVector: ImageVector? = null
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .let {
+                if (onClick != null) {
+                    it.clickable { onClick() }
+                } else {
+                    it
+                }
+            }
+            .padding(padding),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        imageUrl?.let {
+            val model = rememberAsyncImagePainter(imageUrl)
+            val transition by animateFloatAsState(
+                targetValue = if (model.state is AsyncImagePainter.State.Success) 1f else 0f
+            )
+            Box(
+                modifier = Modifier
+                    .size(imageSize)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.surface),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .let {
+                            if (model.state !is AsyncImagePainter.State.Success) {
+                                it.border(0.5.dp, MaterialTheme.colorScheme.onSurface, CircleShape)
+                            } else it
+                        }
+                        .alpha(transition),
+                    contentScale = ContentScale.Crop,
+                    painter = model,
+                    contentDescription = null,
+                )
+                when (model.state) {
+                    is AsyncImagePainter.State.Loading -> {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(16.dp),
+                            strokeWidth = 2.dp,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+
+                    is AsyncImagePainter.State.Error, AsyncImagePainter.State.Empty -> {
+                        Text(
+                            text = title.first().toString(),
+                            fontSize = imageUrlErrorTextSize.sp,
+                            fontWeight = FontWeight.Bold,
+                            maxLines = 1,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+
+                    else -> {
+
+                    }
+                }
+            }
+        } ?: imageVector?.let {
+            Icon(
+                modifier = Modifier.size(imageSize),
+                imageVector = imageVector,
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = title,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            if (subtitle != null) {
+                Text(
+                    text = subtitle,
+                    fontSize = 12.sp,
+                    maxLines = subtitleMaxLines,
+                    overflow = TextOverflow.Ellipsis,
+                    style = TextStyle(
+                        platformStyle = PlatformTextStyle(
+                            includeFontPadding = false
+                        )
+                    ),
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+        if (onClick != null) {
+            Icon(
+                modifier = Modifier.size(24.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.right),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface
+            )
         }
     }
 }
