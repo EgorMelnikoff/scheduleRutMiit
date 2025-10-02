dfa68768ba009a9f592329e6fc1b275a7270f3a8
egor <goodperson2033@gmail.com>
2025-10-02 22:59:59 +0300

    Added titleText and titleContent, updated ScheduleTopAppBar()

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/TopAppBar.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/TopAppBar.kt
@@ -1,7 +1,12 @@
 package com.egormelnikoff.schedulerutmiit.ui.elements
 
+import android.content.Context
 import android.content.Intent
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,22 +16,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconToggleButton
-import androidx.compose.material3.IconToggleButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -34,30 +34,33 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import com.egormelnikoff.schedulerutmiit.R
-import com.egormelnikoff.schedulerutmiit.data.datasource.datastore.DataStore
+import com.egormelnikoff.schedulerutmiit.data.entity.NamedScheduleEntity
 import com.egormelnikoff.schedulerutmiit.data.entity.ScheduleEntity
-import com.egormelnikoff.schedulerutmiit.ui.view_models.ScheduleUiState
-import com.egormelnikoff.schedulerutmiit.ui.view_models.ScheduleViewModel
-import kotlinx.coroutines.launch
+import com.egormelnikoff.schedulerutmiit.ui.view_models.schedule.ScheduleUiState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SimpleTopBar(
-    title: String,
+fun CustomTopAppBar(
+    titleContent: @Composable (() -> Unit)? = null,
+    titleText: String? = null,
     navAction: (() -> Unit)? = null,
-    navImageVector: ImageVector,
+    navImageVector: ImageVector = ImageVector.vectorResource(R.drawable.back),
     actions: @Composable (() -> Unit)? = null,
 ) {
     TopAppBar(
         title = {
-            Text(
-                text = title,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                color = MaterialTheme.colorScheme.onBackground
-            )
+            titleText?.let {
+                Text(
+                    text = titleText,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            } ?: titleContent?.let {
+                titleContent.invoke()
+            }
         },
         navigationIcon = {
             if (navAction != null) {
@@ -82,83 +85,56 @@ fun SimpleTopBar(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScheduleTopBar(
-    scheduleUiState: ScheduleUiState,
-    scheduleViewModel: ScheduleViewModel,
-
+fun ScheduleTopAppBar (
     navigateToAddEvent: (ScheduleEntity) -> Unit,
-
-    onShowExpandedMenu: (Boolean) -> Unit,
+    scheduleUiState: ScheduleUiState,
+    calendarView: Boolean,
+    context: Context,
     expandedSchedulesMenu: Boolean,
-
-    onLoadDefaultSchedule: () -> Unit,
-    onShowLoadDefaultScheduleButton: Boolean,
-    preferencesDataStore: DataStore,
-    isScheduleCalendar: Boolean
+    onShowExpandedMenu: (Boolean) -> Unit,
+    onSetScheduleView: (Boolean) -> Unit,
+    onShowNamedScheduleDialog: (NamedScheduleEntity) -> Unit,
+    onSaveCurrentNamedSchedule: () -> Unit
 ) {
-    val isNotEmpty = scheduleUiState.currentNamedSchedule!!.schedules.isNotEmpty() && scheduleUiState.currentScheduleEntity != null
-    val isSomeSchedules = scheduleUiState.currentNamedSchedule.schedules.size > 1
-    val isCustomSchedule = scheduleUiState.currentNamedSchedule.namedScheduleEntity.type == 3
-
-    val scope = rememberCoroutineScope()
-    val context = LocalContext.current
-
+    val isNotEmpty = scheduleUiState.currentScheduleData!!.namedSchedule!!.schedules.isNotEmpty() && scheduleUiState.currentScheduleData.settledScheduleEntity != null
+    val isSomeSchedules = scheduleUiState.currentScheduleData.namedSchedule!!.schedules.size > 1
+    val isCustomSchedule = scheduleUiState.currentScheduleData.namedSchedule.namedScheduleEntity.type == 3
     val rotationAngle by animateFloatAsState(
         targetValue = if (expandedSchedulesMenu) 180f else 0f
     )
 
-
-    TopAppBar(
-        navigationIcon = {
-            if (onShowLoadDefaultScheduleButton) {
-                IconButton(
-                    onClick = {
-                        onLoadDefaultSchedule()
-                    }
-                ) {
-                    Icon(
-                        modifier = Modifier
-                            .size(24.dp),
-                        imageVector = ImageVector.vectorResource(R.drawable.back),
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onBackground
-                    )
-                }
-            }
-        },
-        title = {
+    CustomTopAppBar(
+        titleContent = {
             Row(
-                modifier = Modifier
-                    .let {
-                        if (isSomeSchedules) {
-                            it
-                                .clip(RoundedCornerShape(8.dp))
-                                .clickable(
-                                    onClick = {
-                                        onShowExpandedMenu(!expandedSchedulesMenu)
-                                    }
-                                )
-                        } else {
-                            it
-                        }
-                    },
+                modifier = Modifier.let {
+                    if (isSomeSchedules) {
+                        it
+                            .clip(RoundedCornerShape(8.dp))
+                            .clickable(
+                                onClick = {
+                                    onShowExpandedMenu(!expandedSchedulesMenu)
+                                }
+                            )
+                    } else {
+                        it
+                    }
+                },
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(4.dp)
             ) {
                 Column {
                     Text(
-                        text = scheduleUiState.currentNamedSchedule.namedScheduleEntity.shortName,
+                        text = scheduleUiState.currentScheduleData.namedSchedule.namedScheduleEntity.shortName,
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
                         color = MaterialTheme.colorScheme.onBackground
                     )
-                    if (scheduleUiState.currentScheduleEntity != null && !isCustomSchedule) {
+                    if (scheduleUiState.currentScheduleData.settledScheduleEntity != null && !isCustomSchedule) {
                         Text(
-                            text = scheduleUiState.currentScheduleEntity.typeName,
+                            text = scheduleUiState.currentScheduleData.settledScheduleEntity.typeName,
                             fontSize = 12.sp,
                             maxLines = 1,
                             overflow = TextOverflow.Ellipsis,
@@ -182,7 +158,7 @@ fun ScheduleTopBar(
             if (isNotEmpty && !isCustomSchedule) {
                 IconButton(
                     onClick = {
-                        val url = scheduleUiState.currentScheduleEntity?.downloadUrl
+                        val url = scheduleUiState.currentScheduleData.settledScheduleEntity?.downloadUrl
                         val intent = Intent(Intent.ACTION_VIEW, url?.toUri())
                         context.startActivity(intent)
                     }
@@ -199,7 +175,7 @@ fun ScheduleTopBar(
             if (isNotEmpty && isCustomSchedule) {
                 IconButton(
                     onClick = {
-                        navigateToAddEvent(scheduleUiState.currentScheduleEntity!!)
+                        navigateToAddEvent(scheduleUiState.currentScheduleData.settledScheduleEntity!!)
                     }
                 ) {
                     Icon(
@@ -212,61 +188,58 @@ fun ScheduleTopBar(
             }
             IconButton(
                 onClick = {
-                    scope.launch {
-                        if (isScheduleCalendar) {
-                            preferencesDataStore.setScheduleView(false)
-                        } else {
-                            preferencesDataStore.setScheduleView(true)
-                        }
+                    if (calendarView) {
+                        onSetScheduleView(false)
+                    } else {
+                        onSetScheduleView(true)
                     }
-
                 }
             ) {
                 Icon(
                     modifier = Modifier.size(24.dp),
-                    imageVector = if (isScheduleCalendar) ImageVector.vectorResource(R.drawable.list) else ImageVector.vectorResource(
+                    imageVector = if (calendarView) ImageVector.vectorResource(R.drawable.list) else ImageVector.vectorResource(
                         R.drawable.calendar
                     ),
                     contentDescription = null,
                     tint = MaterialTheme.colorScheme.onBackground
                 )
             }
-            IconToggleButton(
-                checked = scheduleUiState.isSaved,
-                enabled = if (scheduleUiState.isSaved) true else scheduleUiState.isSavingAvailable,
-                colors = IconToggleButtonColors(
-                    containerColor = Color.Unspecified,
-                    checkedContainerColor = Color.Unspecified,
-                    disabledContainerColor = Color.Unspecified,
-
-                    contentColor = MaterialTheme.colorScheme.onBackground,
-                    checkedContentColor = MaterialTheme.colorScheme.primary,
-                    disabledContentColor = MaterialTheme.colorScheme.outline
-                ),
-                onCheckedChange = {
-                    if (scheduleUiState.isSaved) {
-                        scheduleViewModel.deleteNamedSchedule(
-                            primaryKeyNamedSchedule = scheduleUiState.currentNamedSchedule.namedScheduleEntity.id,
-                            isDefault = scheduleUiState.currentNamedSchedule.namedScheduleEntity.isDefault
+            AnimatedContent(
+                targetState = scheduleUiState.isSaved,
+                transitionSpec = {
+                    fadeIn() togetherWith fadeOut()
+                }
+            ) { isSaved ->
+                if (isSaved) {
+                    IconButton(
+                        onClick = {
+                            onShowNamedScheduleDialog(
+                                scheduleUiState.currentScheduleData.namedSchedule.namedScheduleEntity
+                            )
+                        },
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = ImageVector.vectorResource(R.drawable.more_vert),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onBackground
                         )
-                    } else {
-                        scheduleViewModel.saveCurrentNamedSchedule()
+                    }
+                } else {
+                    IconButton(
+                        onClick = {
+                            onSaveCurrentNamedSchedule()
+                        }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = ImageVector.vectorResource(R.drawable.save),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
                     }
                 }
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .size(24.dp),
-                    imageVector = if (scheduleUiState.isSaved) ImageVector.vectorResource(R.drawable.save_fill) else ImageVector.vectorResource(
-                        R.drawable.save
-                    ),
-                    contentDescription = null
-                )
             }
-        },
-        colors = TopAppBarDefaults.topAppBarColors().copy(
-            containerColor = MaterialTheme.colorScheme.background,
-            scrolledContainerColor = MaterialTheme.colorScheme.background
-        )
+        }
     )
 }
