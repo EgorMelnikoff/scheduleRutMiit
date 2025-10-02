c5c994b740513866706d87df6a41a1299de85e8d
egor <goodperson2033@gmail.com>
2025-10-02 22:31:15 +0300

    Added nullable title

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/screens/Error.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/screens/Error.kt
@@ -4,22 +4,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -30,14 +22,11 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun ErrorScreen(
-    title: String,
+    title: String? = null,
     subtitle: String? = null,
-    buttonTitle: String? = null,
-    imageVector: ImageVector? = null,
-    action: (() -> Unit)? = null,
+    button: @Composable (() -> Unit)? = null,
     paddingTop: Dp? = null,
-    paddingBottom: Dp? = null
-
+    paddingBottom: Dp? = null,
 ) {
     Box(
         modifier = Modifier
@@ -54,18 +43,20 @@ fun ErrorScreen(
         Column(
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Start,
-                text = title,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onBackground
-            )
+            if (title != null) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
             if (subtitle != null) {
                 Text(
                     modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Start,
+                    textAlign = TextAlign.Center,
                     text = subtitle,
                     style = TextStyle(
                         platformStyle = PlatformTextStyle(
@@ -76,29 +67,7 @@ fun ErrorScreen(
                     color = MaterialTheme.colorScheme.onSurface
                 )
             }
-            if (action != null) {
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = action,
-                    shape = RoundedCornerShape(16.dp),
-                    colors = ButtonDefaults.buttonColors().copy(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        contentColor = MaterialTheme.colorScheme.onPrimary
-                    )
-                ) {
-                    Icon(
-                        modifier = Modifier.size(16.dp),
-                        imageVector = imageVector!!,
-                        contentDescription = null
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = buttonTitle!!,
-                        fontSize = 12.sp,
-                        color = MaterialTheme.colorScheme.onPrimary
-                    )
-                }
-            }
+            button?.invoke()
         }
     }
 }
