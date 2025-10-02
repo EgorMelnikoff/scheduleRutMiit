c8d7d94a5d9dfc8f3f21d367ad7fad576b6ae776
egor <goodperson2033@gmail.com>
2025-10-02 22:49:34 +0300

    Replaced Link to ClickableItem and deleted Link()

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/dialogs/InfoDialog.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/dialogs/InfoDialog.kt
@@ -1,9 +1,6 @@
 package com.egormelnikoff.schedulerutmiit.ui.dialogs
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,17 +20,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
@@ -42,35 +35,33 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.pm.PackageInfoCompat.getLongVersionCode
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
 import com.egormelnikoff.schedulerutmiit.R
 import com.egormelnikoff.schedulerutmiit.data.datasource.remote.parser.ParserRoutes.APP_CHANNEL_URL
 import com.egormelnikoff.schedulerutmiit.data.datasource.remote.parser.ParserRoutes.APP_GITHUB_REPOS
 import com.egormelnikoff.schedulerutmiit.data.datasource.remote.parser.ParserRoutes.CLOUD_TIPS
+import com.egormelnikoff.schedulerutmiit.ui.elements.ClickableItem
 import com.egormelnikoff.schedulerutmiit.ui.elements.ColumnGroup
-import com.egormelnikoff.schedulerutmiit.ui.elements.SimpleTopBar
-import com.egormelnikoff.schedulerutmiit.ui.view_models.AppInfoState
+import com.egormelnikoff.schedulerutmiit.ui.elements.CustomTopAppBar
+import com.egormelnikoff.schedulerutmiit.ui.view_models.settings.AppInfoState
 
 @Composable
 fun InfoDialog(
+    externalPadding: PaddingValues,
     onBack: () -> Unit,
-    appInfoState: AppInfoState,
-    paddingValues: PaddingValues,
-    uriHandler: UriHandler
+    onOpenUri: (String) -> Unit,
+    appInfoState: AppInfoState
 ) {
     val context = LocalContext.current
     val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
 
     Scaffold(
         topBar = {
-            SimpleTopBar(
-                title = LocalContext.current.getString(R.string.about_app),
-                navAction = { onBack() },
-                navImageVector = ImageVector.vectorResource(R.drawable.back)
+            CustomTopAppBar(
+                titleText = LocalContext.current.getString(R.string.about_app),
+                navAction = { onBack() }
             )
         }
-    ) { padding ->
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -78,8 +69,8 @@ fun InfoDialog(
                 .padding(
                     start = 16.dp,
                     end = 16.dp,
-                    top = padding.calculateTopPadding(),
-                    bottom = paddingValues.calculateBottomPadding()
+                    top = innerPadding.calculateTopPadding(),
+                    bottom = externalPadding.calculateBottomPadding()
                 )
                 .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -129,36 +120,23 @@ fun InfoDialog(
             ColumnGroup(
                 items = listOf(
                     {
-                        Link(
-                            icon = {
-                                Icon(
-                                    modifier = Modifier.size(36.dp),
-                                    imageVector = ImageVector.vectorResource(R.drawable.logo_telegram),
-                                    contentDescription = null,
-                                    tint = Color.Unspecified
-                                )
-                            },
+                        ClickableItem(
                             title = LocalContext.current.getString(R.string.telegram),
                             subtitle = LocalContext.current.getString(R.string.news_updates),
+                            imageVector = ImageVector.vectorResource(R.drawable.logo_telegram),
                             onClick = {
-                                uriHandler.openUri(APP_CHANNEL_URL)
+                                onOpenUri(APP_CHANNEL_URL)
                             }
                         )
                     }, {
-                        Link(
-                            icon = {
-                                Icon(
-                                    modifier = Modifier.size(36.dp),
-                                    imageVector = ImageVector.vectorResource(R.drawable.logo_github),
-                                    contentDescription = null,
-                                    tint = MaterialTheme.colorScheme.onBackground
-                                )
-                            },
+                        ClickableItem(
                             title = LocalContext.current.getString(R.string.github),
                             subtitle = LocalContext.current.getString(R.string.source_code),
+                            imageVector = ImageVector.vectorResource(R.drawable.logo_github),
                             onClick = {
-                                uriHandler.openUri(APP_GITHUB_REPOS)
-                            }
+                                onOpenUri(APP_GITHUB_REPOS)
+                            },
+
                         )
                     }
                 )
@@ -166,18 +144,11 @@ fun InfoDialog(
 
             ColumnGroup(
                 items = listOf {
-                    Link(
-                        icon = {
-                            Icon(
-                                modifier = Modifier.size(36.dp),
-                                imageVector = ImageVector.vectorResource(R.drawable.ruble),
-                                contentDescription = null,
-                                tint = MaterialTheme.colorScheme.onBackground
-                            )
-                        },
+                    ClickableItem(
                         title = LocalContext.current.getString(R.string.support),
+                        imageVector = ImageVector.vectorResource(R.drawable.ruble),
                         onClick = {
-                            uriHandler.openUri(CLOUD_TIPS)
+                            onOpenUri(CLOUD_TIPS)
                         }
                     )
                 }
@@ -201,30 +172,13 @@ fun InfoDialog(
                     if (appInfoState.authorTelegramPage != null) {
                         ColumnGroup(
                             items = listOf {
-                                Link(
-                                    icon = {
-                                        val model =
-                                            rememberAsyncImagePainter(appInfoState.authorTelegramPage.imageUrl!!)
-                                        val transition by animateFloatAsState(
-                                            targetValue = if (model.state is AsyncImagePainter.State.Success) 1f else 0f
-                                        )
-                                        Image(
-                                            modifier = Modifier
-                                                .size(36.dp)
-                                                .clip(CircleShape)
-                                                .alpha(transition),
-                                            contentScale = ContentScale.Crop,
-                                            painter = model,
-                                            contentDescription = null,
-                                        )
-                                    },
+                                ClickableItem(
                                     title = appInfoState.authorTelegramPage.name!!,
                                     subtitle = LocalContext.current.getString(R.string.author),
+                                    imageUrl = appInfoState.authorTelegramPage.imageUrl!!,
                                     onClick = {
                                         appInfoState.authorTelegramPage.url?.let {
-                                            uriHandler.openUri(
-                                                it
-                                            )
+                                            onOpenUri(it)
                                         }
                                     }
                                 )
@@ -234,59 +188,5 @@ fun InfoDialog(
                 }
             }
         }
-    }
-}
-
-
-@Composable
-fun Link(
-    icon: @Composable (() -> Unit),
-    title: String,
-    subtitle: String? = null,
-    onClick: () -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(
-                onClick = onClick
-            )
-            .padding(horizontal = 8.dp, vertical = 12.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        icon.invoke()
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Text(
-                text = title,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            if (subtitle != null) {
-                Text(
-                    text = subtitle,
-                    fontSize = 12.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    style = TextStyle(
-                        platformStyle = PlatformTextStyle(
-                            includeFontPadding = false
-                        )
-                    ),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-            }
-        }
-        Icon(
-            modifier = Modifier.size(24.dp),
-            imageVector = ImageVector.vectorResource(R.drawable.right),
-            contentDescription = null,
-            tint = MaterialTheme.colorScheme.onSurface
-        )
     }
 }
