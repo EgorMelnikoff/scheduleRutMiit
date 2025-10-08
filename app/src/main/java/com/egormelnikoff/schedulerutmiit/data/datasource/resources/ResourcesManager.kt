dc11ac7d8e10bd81a4d0ced6403859ba3d61ab9a
egor <goodperson2033@gmail.com>
2025-10-08 14:07:39 +0300

    Used Dagger

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/data/datasource/resources/ResourcesManager.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/data/datasource/resources/ResourcesManager.kt
@@ -2,13 +2,16 @@ package com.egormelnikoff.schedulerutmiit.data.datasource.resources
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import javax.inject.Inject
 
-interface ResourcesManager{
+interface ResourcesManager {
     fun getString(id: Int): String?
     fun getDrawable(id: Int): Drawable?
 }
 
-class ResourcesManagerImpl (private  val context: Context): ResourcesManager {
+class ResourcesManagerImpl @Inject constructor(
+    private val context: Context
+) : ResourcesManager {
     override fun getString(id: Int) = context.getString(id)
     override fun getDrawable(id: Int) = context.getDrawable(id)
 }
