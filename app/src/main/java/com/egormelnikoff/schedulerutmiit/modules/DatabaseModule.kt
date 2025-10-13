278f3b0945d4cb2720616bc2dee871f1076b83a7
egor <goodperson2033@gmail.com>
2025-10-13 18:11:42 +0300

    Added provideConverters()

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/modules/DatabaseModule.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/modules/DatabaseModule.kt
@@ -1,8 +1,10 @@
 package com.egormelnikoff.schedulerutmiit.modules
 
 import android.content.Context
-import com.egormelnikoff.schedulerutmiit.data.datasource.local.AppDatabase
-import com.egormelnikoff.schedulerutmiit.data.datasource.local.NamedScheduleDao
+import com.egormelnikoff.schedulerutmiit.data.datasource.local.database.AppDatabase
+import com.egormelnikoff.schedulerutmiit.data.datasource.local.database.Converters
+import com.egormelnikoff.schedulerutmiit.data.datasource.local.database.NamedScheduleDao
+import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,16 +15,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
-
     @Provides
     @Singleton
-    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
-        return AppDatabase.getDatabase(context)
+    fun provideAppDatabase(@ApplicationContext context: Context, converters: Converters): AppDatabase {
+        return AppDatabase.getDatabase(context, converters)
     }
 
     @Provides
     @Singleton
     fun provideNamedScheduleDao(database: AppDatabase): NamedScheduleDao {
         return database.namedScheduleDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverters(gson: Gson): Converters {
+        return Converters(gson)
     }
 }
