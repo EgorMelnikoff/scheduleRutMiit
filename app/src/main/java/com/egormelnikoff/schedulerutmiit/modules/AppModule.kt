d11c1dd617b2c04996988474dd5e00b600693666
egor <goodperson2033@gmail.com>
2025-10-13 18:10:42 +0300

    Added provideGson()

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/modules/AppModule.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/modules/AppModule.kt
@@ -1,14 +1,22 @@
 package com.egormelnikoff.schedulerutmiit.modules
 
 import android.content.Context
-import com.egormelnikoff.schedulerutmiit.data.datasource.datastore.PreferencesDataStore
-import com.egormelnikoff.schedulerutmiit.data.datasource.resources.ResourcesManager
-import com.egormelnikoff.schedulerutmiit.data.datasource.resources.ResourcesManagerImpl
+import com.egormelnikoff.schedulerutmiit.data.datasource.local.prefs_datastore.PreferencesDataStore
+import com.egormelnikoff.schedulerutmiit.data.datasource.local.resources.ResourcesManager
+import com.egormelnikoff.schedulerutmiit.data.datasource.local.resources.ResourcesManagerImpl
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Singleton
 
 @Module
@@ -24,5 +32,34 @@ object AppModule {
     @Singleton
     fun provideResourcesManager(@ApplicationContext context: Context): ResourcesManager {
         return ResourcesManagerImpl(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder()
+            .registerTypeAdapter(
+                LocalDate::class.java,
+                JsonDeserializer { json, _, _ ->
+                    LocalDate.parse(json.asString, DateTimeFormatter.ISO_DATE)
+                }
+            )
+            .registerTypeAdapter(
+                LocalDate::class.java,
+                JsonSerializer<LocalDate> { src, _, _ ->
+                    JsonPrimitive(DateTimeFormatter.ISO_DATE.format(src))
+                }
+            )
+            .registerTypeAdapter(
+                LocalDateTime::class.java,
+                JsonDeserializer { json, _, _ ->
+                    LocalDateTime.parse(json.asString, DateTimeFormatter.ISO_ZONED_DATE_TIME)
+                }
+            ).registerTypeAdapter(
+                LocalDateTime::class.java,
+                JsonSerializer<LocalDateTime> { src, _, _ ->
+                    JsonPrimitive(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(src))
+                }
+            ).create()
     }
 }
