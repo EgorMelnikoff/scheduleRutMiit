3a76083e644b04fe11a4de4efafa14ff5dd58e84
egor <goodperson2033@gmail.com>
2025-10-13 18:20:46 +0300

    Removed gson from NetworkModule.kt

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/modules/NetworkModule.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/modules/NetworkModule.kt
@@ -1,10 +1,8 @@
 package com.egormelnikoff.schedulerutmiit.modules
 
-import com.egormelnikoff.schedulerutmiit.data.datasource.remote.api.Api
-import com.egormelnikoff.schedulerutmiit.data.datasource.remote.api.ApiRoutes.BASE_URL
+import com.egormelnikoff.schedulerutmiit.data.datasource.remote.api.MiitApi
+import com.egormelnikoff.schedulerutmiit.data.datasource.remote.api.MiitApiRoutes.BASE_URL
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonDeserializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,33 +10,12 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import javax.inject.Singleton
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-    @Provides
-    @Singleton
-    fun provideGson(): Gson {
-        return GsonBuilder()
-            .registerTypeAdapter(
-                LocalDate::class.java,
-                JsonDeserializer { json, _, _ ->
-                    LocalDate.parse(json.asString, DateTimeFormatter.ISO_DATE)
-                }
-            )
-            .registerTypeAdapter(
-                LocalDateTime::class.java,
-                JsonDeserializer { json, _, _ ->
-                    LocalDateTime.parse(json.asString, DateTimeFormatter.ISO_ZONED_DATE_TIME)
-                }
-            ).create()
-    }
-
     @Provides
     @Singleton
     fun provideOkHttpClient(): OkHttpClient {
@@ -49,7 +26,10 @@ object NetworkModule {
             .callTimeout(60, TimeUnit.SECONDS)
             .addInterceptor { chain ->
                 val newRequest = chain.request().newBuilder()
-                    .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+                    .addHeader(
+                        "User-Agent",
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+                    )
                     .build()
                 chain.proceed(newRequest)
             }
@@ -68,7 +48,7 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideApiService(retrofit: Retrofit): Api {
-        return retrofit.create(Api::class.java)
+    fun provideApiService(retrofit: Retrofit): MiitApi {
+        return retrofit.create(MiitApi::class.java)
     }
 }
