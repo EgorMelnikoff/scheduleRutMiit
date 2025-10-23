fdace7eb056bd30f4bd69063db441d9aaa746e85
egor <goodperson2033@gmail.com>
2025-10-23 19:41:21 +0300

    Added logger

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/app/widget/WidgetUpdater.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/app/widget/WidgetUpdater.kt
@@ -5,6 +5,7 @@ import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.work.ListenableWorker
+import com.egormelnikoff.schedulerutmiit.app.logger.Logger
 import com.egormelnikoff.schedulerutmiit.app.widget.ui.EventsWidget
 import com.egormelnikoff.schedulerutmiit.data.repos.schedule.ScheduleRepos
 import com.google.gson.Gson
@@ -17,15 +18,17 @@ interface WidgetDataUpdater {
 class WidgetDataUpdaterImpl @Inject constructor(
     private val scheduleRepos: ScheduleRepos,
     private val context: Context,
-    private val gson: Gson
+    private val gson: Gson,
+    private val logger: Logger
 ) : WidgetDataUpdater {
     override suspend fun updateAll(): ListenableWorker.Result {
         val namedScheduleEntity = scheduleRepos.getDefaultNamedScheduleEntity()
             ?: return ListenableWorker.Result.failure()
-
+        logger.i("WidgetDataUpdater", "Default schedule:\n$namedScheduleEntity")
         val namedSchedule = scheduleRepos.getSavedNamedScheduleById(namedScheduleEntity.id)!!
         val widgetData = WidgetData.calculateWidgetData(namedSchedule)
         if (widgetData != null) {
+            logger.i("WidgetDataUpdater", "Start widget update")
             GlanceAppWidgetManager(context).getGlanceIds(EventsWidget::class.java)
                 .forEach { glanceId ->
                     updateWidgetState(glanceId, widgetData)
