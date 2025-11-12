c8ddc511618813426bc0d4762014377bca50d090
egor <goodperson2033@gmail.com>
2025-11-12 17:12:37 +0300

    Deleted visibleHiddenEvents

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/state/ReviewState.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/state/ReviewState.kt
@@ -11,40 +11,32 @@ data class ReviewState(
     val visibleSavedSchedules: Boolean,
     val onChangeVisibilitySavedSchedules: (Boolean) -> Unit,
 
-    val visibleHiddenEvents: Boolean,
-    val onChangeVisibilityHiddenEvents: (Boolean) -> Unit,
+    val searchQuery: String,
+    val onChangeQuery: (String) -> Unit,
 
     val selectedSearchOption: SearchOption,
     val onSelectSearchOption: (SearchOption) -> Unit,
-
-    val searchQuery: String,
-    val onChangeQuery: (String) -> Unit
 )
 
 @Composable
 fun rememberReviewState(): ReviewState {
     var visibleSavedSchedules by remember { mutableStateOf(true) }
-    var visibleHiddenEvents by remember { mutableStateOf(false) }
 
-    var selectedSearchOption by remember { mutableStateOf(SearchOption.ALL) }
     var searchQuery by remember { mutableStateOf("") }
+    var selectedSearchOption by remember { mutableStateOf(SearchOption.ALL) }
 
     return ReviewState(
         visibleSavedSchedules = visibleSavedSchedules,
         onChangeVisibilitySavedSchedules = { newValue ->
             visibleSavedSchedules = newValue
         },
-        visibleHiddenEvents = visibleHiddenEvents,
-        onChangeVisibilityHiddenEvents = { newValue ->
-            visibleHiddenEvents = newValue
+        searchQuery = searchQuery,
+        onChangeQuery = { newValue ->
+            searchQuery = newValue
         },
         selectedSearchOption = selectedSearchOption,
         onSelectSearchOption = { newValue ->
             selectedSearchOption = newValue
         },
-        searchQuery = searchQuery,
-        onChangeQuery = { newValue ->
-            searchQuery = newValue
-        }
     )
 }
