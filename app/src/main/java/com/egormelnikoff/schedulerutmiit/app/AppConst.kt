18be345978d971b7839eb6d0a6531944f4e9a0f4
egor <goodperson2033@gmail.com>
2025-11-22 02:04:09 +0300

    Added author name

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/app/AppConst.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/app/AppConst.kt
@@ -1,6 +1,7 @@
 package com.egormelnikoff.schedulerutmiit.app
 
 object AppConst {
+    const val AUTHOR = "Egor Melnikov"
     const val APP_CHANNEL_URL = "https://t.me/schedule_rut_miit"
     const val APP_GITHUB_REPOS = "https://github.com/EgorMelnikoff/scheduleRutMiit"
     const val AUTHOR_CHANNEL_URL = "https://t.me/s/EgorMelnikoff"
