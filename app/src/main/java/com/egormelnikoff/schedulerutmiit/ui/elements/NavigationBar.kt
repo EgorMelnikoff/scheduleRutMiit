9ac409bcfc54b445bb7fa36d36d141601d34c4a7
egor <goodperson2033@gmail.com>
2025-11-26 23:59:28 +0300

    New navigationBar design

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/NavigationBar.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/NavigationBar.kt
@@ -1,22 +1,28 @@
 package com.egormelnikoff.schedulerutmiit.ui.elements
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -26,9 +32,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
@@ -42,31 +51,68 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.egormelnikoff.schedulerutmiit.R
-import com.egormelnikoff.schedulerutmiit.ui.navigation.AppBackStack
-import com.egormelnikoff.schedulerutmiit.ui.navigation.Routes
-import com.egormelnikoff.schedulerutmiit.ui.theme.Grey
+import com.egormelnikoff.schedulerutmiit.data.datasource.local.preferences.AppSettings
+import com.egormelnikoff.schedulerutmiit.ui.navigation.Route
+import com.egormelnikoff.schedulerutmiit.ui.state.AppUiState
+import com.egormelnikoff.schedulerutmiit.ui.state.ScheduleUiState
+import com.egormelnikoff.schedulerutmiit.ui.theme.color.Grey
+import com.egormelnikoff.schedulerutmiit.ui.theme.isDarkTheme
+import com.egormelnikoff.schedulerutmiit.view_models.schedule.ScheduleState
+import kotlinx.coroutines.launch
 
 data class BarItem(
     val title: String,
     val icon: ImageVector,
     val selectedIcon: ImageVector,
-    val route: Routes,
+    val route: Route.Page,
     val onClick: (() -> Unit)?
 )
 
 @Composable
 fun CustomNavigationBar(
-    appBackStack: AppBackStack<Routes>,
-    barItems: Array<BarItem>,
-    theme: String
+    appUiState: AppUiState,
+    scheduleState: ScheduleState,
+    scheduleUiState: ScheduleUiState?,
+    appSettings: AppSettings
 ) {
-    val darkTheme = when (theme) {
-        "dark" -> true
-        "light" -> false
-        else -> {
-            isSystemInDarkTheme()
+    val barItems = barItems(
+        onScheduleClick = {
+            appUiState.scope.launch {
+                when {
+                    scheduleUiState != null && scheduleState.currentNamedScheduleData?.scheduleData?.schedulePagerData != null && appSettings.calendarView -> {
+                        scheduleUiState.onSelectDate(
+                            scheduleState.currentNamedScheduleData.scheduleData.schedulePagerData.defaultDate
+                        )
+                        scheduleUiState.pagerWeeksState.animateScrollToPage(
+                            scheduleState.currentNamedScheduleData.scheduleData.schedulePagerData.weeksStartIndex
+                        )
+                    }
+
+                    scheduleUiState != null && !appSettings.calendarView -> {
+                        scheduleUiState.scheduleListState.animateScrollToItem(0)
+                    }
+                }
+            }
+        },
+        onNewsClick = {
+            appUiState.scope.launch {
+                appUiState.newsListState.animateScrollToItem(0)
+            }
+        },
+        onSettingsClick = {
+            appUiState.scope.launch {
+                appUiState.settingsListState.animateScrollToItem(0)
+            }
         }
-    }
+    )
+
+    var selectedIndex by remember { mutableIntStateOf(1) }
+
+    val indicatorOffset by animateDpAsState(
+        targetValue = 68.dp * selectedIndex,
+        animationSpec = tween(200, easing = LinearOutSlowInEasing)
+    )
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -88,13 +134,13 @@ fun CustomNavigationBar(
             ),
         contentAlignment = Alignment.Center
     ) {
-        Row(
+        Box(
             modifier = Modifier
                 .clickable(
                     enabled = false
                 ) {}
                 .let {
-                    if (darkTheme) {
+                    if (appSettings.theme.isDarkTheme()) {
                         it.border(
                             width = 0.5.dp,
                             shape = CircleShape,
@@ -111,7 +157,7 @@ fun CustomNavigationBar(
                             shape = CircleShape,
                             shadow = Shadow(
                                 radius = 8.dp,
-                                color = Grey.copy(0.5f),
+                                color = Grey.copy(0.4f),
                                 offset = DpOffset(2.dp, 2.dp)
                             )
                         )
@@ -121,26 +167,41 @@ fun CustomNavigationBar(
                     color = MaterialTheme.colorScheme.background,
                     shape = CircleShape
                 )
-                .padding(12.dp),
-            verticalAlignment = Alignment.Bottom,
-            horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally)
+                .height(IntrinsicSize.Min)
+                .padding(4.dp),
+            contentAlignment = Alignment.Center
         ) {
-            barItems.forEach { barItem ->
-                CustomNavigationItem(
-                    barItem = barItem,
-                    isSelected = appBackStack.lastPage() == barItem.route,
-                    onClick = {
-                        if (barItem.route == appBackStack.lastPage()) {
-                            if (appBackStack.last().isDialog) {
-                                appBackStack.onBack()
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .offset(x = indicatorOffset)
+                    .width(68.dp)
+                    .align(Alignment.CenterStart)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.8f))
+            )
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                barItems.forEachIndexed { index, barItem ->
+                    CustomNavigationItem(
+                        barItem = barItem,
+                        isSelected = appUiState.appBackStack.lastPage() == barItem.route,
+                        onClick = {
+                            if (barItem.route == appUiState.appBackStack.lastPage()) {
+                                if (appUiState.appBackStack.last() is Route.Dialog) {
+                                    appUiState.appBackStack.onBack()
+                                } else {
+                                    barItem.onClick?.invoke()
+                                }
                             } else {
-                                barItem.onClick?.invoke()
+                                appUiState.appBackStack.navigateToPage(barItem.route)
+                                selectedIndex = index
                             }
-                        } else {
-                            appBackStack.navigateToPage(barItem.route)
                         }
-                    }
-                )
+                    )
+                }
             }
         }
     }
@@ -169,6 +230,7 @@ fun CustomNavigationItem(
                 onClick = onClick
             )
             .scale(scale)
+            .padding(8.dp)
             .width(52.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(4.dp)
@@ -194,7 +256,7 @@ fun CustomNavigationItem(
 
 
 @Composable
-fun rememberBarItems(
+fun barItems(
     onScheduleClick: () -> Unit,
     onNewsClick: () -> Unit,
     onSettingsClick: () -> Unit
@@ -204,28 +266,28 @@ fun rememberBarItems(
             title = LocalContext.current.getString(R.string.review),
             icon = ImageVector.vectorResource(R.drawable.review),
             selectedIcon = ImageVector.vectorResource(R.drawable.review_fill),
-            route = Routes.Review,
+            route = Route.Page.Review,
             onClick = null
         ),
         BarItem(
             title = LocalContext.current.getString(R.string.schedule),
             icon = ImageVector.vectorResource(R.drawable.schedule),
             selectedIcon = ImageVector.vectorResource(R.drawable.schedule_fill),
-            route = Routes.Schedule,
+            route = Route.Page.Schedule,
             onClick = onScheduleClick
         ),
         BarItem(
             title = LocalContext.current.getString(R.string.news),
             icon = ImageVector.vectorResource(R.drawable.news),
             selectedIcon = ImageVector.vectorResource(R.drawable.news_fill),
-            route = Routes.NewsList,
+            route = Route.Page.NewsList,
             onClick = onNewsClick
         ),
         BarItem(
             title = LocalContext.current.getString(R.string.settings),
             icon = ImageVector.vectorResource(R.drawable.settings),
             selectedIcon = ImageVector.vectorResource(R.drawable.settings_fill),
-            route = Routes.Settings,
+            route = Route.Page.Settings,
             onClick = onSettingsClick
         )
     )
