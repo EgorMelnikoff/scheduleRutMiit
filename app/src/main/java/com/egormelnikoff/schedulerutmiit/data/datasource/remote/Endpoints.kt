0829183a99894946c7d120b32d99e61d9043ec97
egor <goodperson2033@gmail.com>
2026-01-30 11:35:37 +0300

    Used repeat in ApiHelper, moved endpoints to Endpoints.kt, added parseListSubjects() in Parser, created NetworkLogger.kt

---
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/data/datasource/remote/Endpoints.kt
@@ -0,0 +1,28 @@
+package com.egormelnikoff.schedulerutmiit.data.datasource.remote
+
+object Endpoints {
+    //API
+    const val BASE_RUT_MIIT_URL = "https://rut-miit.ru/"
+    private const val DATA_SERVICE = "data-service/data/"
+
+    const val GROUPS = "${DATA_SERVICE}timetable/groups-catalog"
+    const val TIMETABLE = "${DATA_SERVICE}timetable/v2/{type}/{apiId}"
+    const val SCHEDULE = "${DATA_SERVICE}timetable/v2/{type}/{apiId}/{timetableId}"
+
+    const val NEWS_CATALOG = "${DATA_SERVICE}news?idk_information_category=2"
+    const val NEWS = "${DATA_SERVICE}news/{newsId}"
+
+    //PARSER
+    const val BASE_MIIT_URL = "https://www.miit.ru"
+
+    fun curriculumProfessorsUrl(id: String, page: Int) =
+        "${BASE_MIIT_URL}/edu/curriculum/$id/professors?page=$page"
+
+    fun peopleUrl(query: String) = "${BASE_MIIT_URL}/people?query=$query"
+
+    fun timetableUrl(apiId: String) =
+        "${BASE_MIIT_URL}/people/$apiId/timetable"
+
+    fun personImageUrl(personId: Int?, width: Int = 100) =
+        "$BASE_MIIT_URL/content/e$personId.jpg?id_fe=$personId&SWidth=$width"
+}
