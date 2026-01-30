dde776b44c9b94dfd4303c78578c13a3e2442399
egor <goodperson2033@gmail.com>
2026-01-30 12:03:44 +0300

    Removed interface

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/view_models/news/NewsViewModel.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/view_models/news/NewsViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.egormelnikoff.schedulerutmiit.app.model.News
 import com.egormelnikoff.schedulerutmiit.app.model.NewsShort
 import com.egormelnikoff.schedulerutmiit.data.Result
 import com.egormelnikoff.schedulerutmiit.data.TypedError
@@ -15,40 +14,26 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-interface NewsViewModel {
-    val newsState: StateFlow<NewsState>
-    val newsListFlow: Flow<PagingData<NewsShort>>
-    fun getNewsById(id: Long)
-    fun setDefaultNewsState()
-}
-
-data class NewsState(
-    val currentNews: News? = null,
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
-
 @HiltViewModel
-class NewsViewModelImpl @Inject constructor(
+class NewsViewModel @Inject constructor(
     private val newsRepos: NewsRepos,
     private val resourcesManager: ResourcesManager
-) : ViewModel(), NewsViewModel {
+) : ViewModel() {
     private val _newsState = MutableStateFlow(NewsState())
-    override val newsState = _newsState.asStateFlow()
+    val newsState = _newsState.asStateFlow()
 
     private var newsJob: Job? = null
 
-    override val newsListFlow: Flow<PagingData<NewsShort>> = newsRepos
+    val newsListFlow: Flow<PagingData<NewsShort>> = newsRepos
         .getNewsListFlow()
         .cachedIn(viewModelScope)
 
-    override fun getNewsById(id: Long) {
+    fun getNewsById(id: Long) {
         _newsState.update { it.copy(isLoading = true) }
         val newNewsJob = viewModelScope.launch {
             newsJob?.cancelAndJoin()
@@ -77,7 +62,7 @@ class NewsViewModelImpl @Inject constructor(
         newsJob = newNewsJob
     }
 
-    override fun setDefaultNewsState() {
+    fun setDefaultNewsState() {
         _newsState.update {
             it.copy(
                 isLoading = false,
