bc583461232c60ca174c8b7696cef77e2ce45f8b
egor <goodperson2033@gmail.com>
2026-01-31 10:11:13 +0300

    Fixed clip

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/ActionDialogButton.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/ActionDialogButton.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
@@ -30,7 +29,6 @@ fun ActionDialogButton(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clip(MaterialTheme.shapes.large)
             .clickable(onClick = onClick)
             .background(containerColor)
             .padding(horizontal = 8.dp, vertical = 12.dp),
