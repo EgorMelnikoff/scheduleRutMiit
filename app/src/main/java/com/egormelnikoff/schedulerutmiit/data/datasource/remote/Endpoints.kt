ad8cc536449cec832c87a1e2418dcc78bb1496b9
egor <goodperson2033@gmail.com>
2026-03-03 17:11:12 +0300

    Changed URLs

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/data/datasource/remote/Endpoints.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/data/datasource/remote/Endpoints.kt
@@ -20,21 +20,20 @@ object Endpoints {
     fun curriculumProfessorsUrl(id: String, page: Int) =
         "${BASE_RUT_MIIT_URL}edu/curriculum/$id/professors?page=$page"
 
-    fun peopleUrl(query: String) = "${BASE_RUT_MIIT_URL}people?query=$query"
+    fun peopleUrl(query: String) =
+        "${BASE_RUT_MIIT_URL}people?query=$query"
 
     fun scheduleUrl(
         namedScheduleType: NamedScheduleType,
         apiId: Int,
         startDate: String,
         type: String,
-    ): String {
-        return when (namedScheduleType) {
-            NamedScheduleType.PERSON -> "${BASE_RUT_MIIT_URL}people/$apiId/timetable?start=$startDate&type=$type"
-            else -> "${BASE_RUT_MIIT_URL}timetable/$apiId?start=$startDate&type=$type"
-        }
+    ) = when (namedScheduleType) {
+        NamedScheduleType.PERSON -> "${BASE_RUT_MIIT_URL}people/$apiId/timetable?start=$startDate&type=$type"
+        else -> "${BASE_RUT_MIIT_URL}timetable/$apiId?start=$startDate&type=$type"
     }
 
 
     fun personImageUrl(personId: Int?, width: Int = 100) =
-        "$BASE_RUT_MIIT_URL}content/e$personId.jpg?id_fe=$personId&SWidth=$width"
+        "$BASE_MIIT_URL/content/e$personId.jpg?id_fe=$personId&SWidth=$width"
 }
