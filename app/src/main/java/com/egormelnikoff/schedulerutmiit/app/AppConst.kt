1618643f496fc1be6ebdd1be86b5e506c1fbea3c
egor <goodperson2033@gmail.com>
2026-03-11 17:26:33 +0300

    Moved URLs

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/app/AppConst.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/app/AppConst.kt
@@ -2,10 +2,5 @@ package com.egormelnikoff.schedulerutmiit.app
 
 object AppConst {
     const val AUTHOR = "Egor Melnikov"
-    const val APP_CHANNEL_URL = "https://t.me/schedule_rut_miit"
-    const val APP_GITHUB_REPOS = "https://github.com/EgorMelnikoff/scheduleRutMiit"
-    const val APP_GITHUB_LATEST_RELEASE = "https://github.com/EgorMelnikoff/scheduleRutMiit/releases/latest"
-    const val RU_STORE = "https://www.rustore.ru/catalog/app/com.egormelnikoff.schedulerutmiit"
-    const val AUTHOR_CHANNEL_URL = "https://t.me/s/EgorMelnikoff"
     const val DEVELOPER_EMAIL = "egormelnikoffdev@gmail.com"
 }
--- app/src/main/java/com/egormelnikoff/schedulerutmiit/data/datasource/remote/Endpoints.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/data/datasource/remote/Endpoints.kt
@@ -36,4 +36,11 @@ object Endpoints {
 
     fun personImageUrl(personId: Int?, width: Int = 100) =
         "$BASE_MIIT_URL/content/e$personId.jpg?id_fe=$personId&SWidth=$width"
+
+
+    const val APP_CHANNEL_URL = "https://t.me/schedule_rut_miit"
+    const val APP_GITHUB_REPOS = "https://github.com/EgorMelnikoff/scheduleRutMiit"
+    const val APP_GITHUB_LATEST_RELEASE = "https://github.com/EgorMelnikoff/scheduleRutMiit/releases/latest"
+    const val RU_STORE = "https://www.rustore.ru/catalog/app/com.egormelnikoff.schedulerutmiit"
+    const val AUTHOR_CHANNEL_URL = "https://t.me/s/EgorMelnikoff"
 }
