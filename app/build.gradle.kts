6f6c29d86bdcce7d93ddbdc20af2dfcdc317ffea
egor <goodperson2033@gmail.com>
2026-03-18 22:17:24 +0300

    New app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.egormelnikoff.schedulerutmiit"
         minSdk = 29
         targetSdk = 36
-        versionCode = 59
-        versionName = "2.1.7"
+        versionCode = 60
+        versionName = "2.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
