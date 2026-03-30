2580f150a4688fabc0821821d25b4724da61c4d6
egor <goodperson2033@gmail.com>
2026-03-30 16:03:03 +0300

    Added enabled param

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/Switch.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/Switch.kt
@@ -12,19 +12,28 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun CustomSwitch (
     checked: Boolean,
+    enabled: Boolean = true,
     colors: SwitchColors? = null,
     onCheckedChange: (Boolean) -> Unit
 ) {
     CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 0.dp) {
         Switch(
             checked = checked,
+            enabled = enabled,
             onCheckedChange = onCheckedChange,
             colors = colors ?: SwitchDefaults.colors().copy(
                 checkedTrackColor = MaterialTheme.colorScheme.primary,
                 checkedThumbColor = MaterialTheme.colorScheme.onPrimary,
                 uncheckedBorderColor = MaterialTheme.colorScheme.onSecondaryContainer,
                 uncheckedThumbColor = MaterialTheme.colorScheme.onSecondaryContainer,
-                uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer
+                uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+
+
+                disabledCheckedTrackColor = MaterialTheme.colorScheme.primary.copy(0.7f),
+                disabledCheckedThumbColor = MaterialTheme.colorScheme.onPrimary.copy(0.9f),
+                disabledUncheckedBorderColor = MaterialTheme.colorScheme.onSecondaryContainer.copy(0.4f),
+                disabledUncheckedThumbColor = MaterialTheme.colorScheme.onSecondaryContainer.copy(0.4f),
+                disabledUncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer.copy(0.7f)
             )
         )
     }
