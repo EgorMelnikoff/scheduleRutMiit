e2fafe4b10398b165d671998e73a51486f1127e7
egor <goodperson2033@gmail.com>
2026-04-06 18:49:37 +0300

    Split parsers

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/datasource/local/parser/SearchParser.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/datasource/local/parser/SearchParser.kt
@@ -22,38 +22,4 @@ object SearchParser {
         }
         return@withContext people
     }
-
-
-    /* Subjects list */
-    suspend fun parsePagesCount(element: Element): Int = withContext(Dispatchers.Default) {
-        return@withContext element.select("ul.pagination li[data-page]")
-            .mapNotNull {
-                it.attr("data-page").toIntOrNull()
-            }
-            .maxOrNull() ?: 1
-    }
-
-    suspend fun parseListSubjectsByPage(
-        element: Element
-    ): MutableMap<String, MutableSet<String>> = withContext(
-        Dispatchers.Default
-    ) {
-        val subjectTeachers = mutableMapOf<String, MutableSet<String>>()
-
-        element.select("div[itemprop=teachingStaff]").forEach { item ->
-            val teacher = item.selectFirst("a[itemprop=fio]")?.text()?.trim()
-            val subjects = item.select("span[itemprop=teachingDiscipline]").mapNotNull {
-                it.text().trim().takeIf { t -> t.isNotBlank() }
-            }
-            if (teacher.isNullOrBlank() || subjects.isEmpty()) {
-                return@forEach
-            }
-            subjects.forEach { subject ->
-                subjectTeachers.getOrPut(subject) {
-                    mutableSetOf()
-                }.add(teacher)
-            }
-        }
-        return@withContext subjectTeachers
-    }
 }
