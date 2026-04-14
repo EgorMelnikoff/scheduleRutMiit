35ea65583aa960fd874012cd56099928b74d178c
egor <goodperson2033@gmail.com>
2026-04-14 17:04:31 +0300

    New logic, used @Compoable content

--- app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/AnimatedAlert.kt
+++ app/src/main/java/com/egormelnikoff/schedulerutmiit/ui/elements/AnimatedAlert.kt
@@ -21,10 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.style.TextOverflow
+
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -33,11 +30,7 @@ fun AnimatedAlert(
     paddingVertical: Dp = 8.dp,
     paddingHorizontal: Dp = 16.dp,
     isHidden: Boolean,
-    title: String,
-    imageVector: ImageVector?,
-    backgroundColor: Color,
-    actionTitle: String? = null,
-    action: (() -> Unit)?
+    content: @Composable () -> Unit
 ) {
     AnimatedVisibility(
         visible = !isHidden,
@@ -50,55 +43,7 @@ fun AnimatedAlert(
                 horizontal = paddingHorizontal
             )
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(MaterialTheme.shapes.medium)
-                    .let {
-                        if (action != null) {
-                            it.clickable(
-                                onClick = action
-                            )
-                        } else it
-                    }
-                    .background(backgroundColor)
-                    .padding(horizontal = 12.dp, vertical = 4.dp)
-                    .defaultMinSize(minHeight = 36.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                imageVector?.let {
-                    Icon(
-                        modifier = Modifier.size(24.dp),
-                        imageVector = imageVector,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onPrimary
-                    )
-                }
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = title,
-                    style = MaterialTheme.typography.titleSmall,
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
-                if (action != null && actionTitle != null) {
-                    CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 0.dp) {
-                        TextButton(
-                            onClick = action,
-                            shape = MaterialTheme.shapes.small,
-                            interactionSource = null
-                        ) {
-                            Text(
-                                text = actionTitle,
-                                style = MaterialTheme.typography.bodyMedium,
-                                color = MaterialTheme.colorScheme.onPrimary
-                            )
-                        }
-                    }
-                }
-            }
+            content.invoke()
         }
     }
 }
