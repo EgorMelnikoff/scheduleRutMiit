b3f4c850618b11ee665e20e4e8e5c28216890d9e
egor <goodperson2033@gmail.com>
2026-04-23 18:44:19 +0300

    Created network modeule

---
+++ core-network/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.library")
+    alias(libs.plugins.devtoolsKsp)
+    alias(libs.plugins.dagger.hilt.android)
+    kotlin("plugin.serialization") version "2.3.20"
+}
+
+android {
+    namespace = "com.egormelnikoff.schedulerutmiit.core.network"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 29
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.androidx.hilt.compiler)
+
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
