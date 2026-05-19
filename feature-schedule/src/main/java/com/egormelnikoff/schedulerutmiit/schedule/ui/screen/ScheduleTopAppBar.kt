35169f51f973d370033db5e4753ac258c9493d0b
egor <goodperson2033@gmail.com>
2026-05-20 00:02:16 +0300

    Added @OptIn(ExperimentalMaterial3Api::class)

--- feature-schedule/src/main/java/com/egormelnikoff/schedulerutmiit/schedule/ui/screen/ScheduleTopAppBar.kt
+++ feature-schedule/src/main/java/com/egormelnikoff/schedulerutmiit/schedule/ui/screen/ScheduleTopAppBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -24,6 +25,7 @@ import com.egormelnikoff.schedulerutmiit.core.common.enums.ScheduleView
 import com.egormelnikoff.schedulerutmiit.core.ui.elements.CustomTopAppBar
 import com.egormelnikoff.schedulerutmiit.schedule.ui.view_model.state.ui_dto.ScheduleUiDto
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScheduleTopAppBar(
     navigateToHiddenEvents: (NamedSchedule) -> Unit,
