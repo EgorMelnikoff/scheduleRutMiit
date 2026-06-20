e4b9a812b70ff507adc031341239b54338711063
egor <goodperson2033@gmail.com>
2026-06-20 23:21:10 +0300

    Used FetchDto

--- core-network/src/main/java/com/egormelnikoff/schedulerutmiit/core/network/api/GithubApi.kt
+++ core-network/src/main/java/com/egormelnikoff/schedulerutmiit/core/network/api/GithubApi.kt
@@ -1,11 +1,11 @@
 package com.egormelnikoff.schedulerutmiit.core.network.api
 
-import com.egormelnikoff.schedulerutmiit.core.common.domain.LatestRelease
+import com.egormelnikoff.schedulerutmiit.core.network.dto.latest_release.LatestReleaseFetchDto
 import com.egormelnikoff.schedulerutmiit.core.network.endpoins.Endpoints
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface GithubApi {
     @GET(Endpoints.APP_GITHUB_API_LATEST_RELEASE)
-    suspend fun getLatestRelease(): Response<LatestRelease>
+    suspend fun getLatestRelease(): Response<LatestReleaseFetchDto>
 }
--- feature-latest-release/src/main/java/com/egormelnikoff/schedulerutmiit/latest_release/data/repos/LatestReleaseDataSourceImpl.kt
+++ feature-latest-release/src/main/java/com/egormelnikoff/schedulerutmiit/latest_release/data/repos/LatestReleaseDataSourceImpl.kt
@@ -4,6 +4,7 @@ import com.egormelnikoff.schedulerutmiit.core.common.domain.LatestRelease
 import com.egormelnikoff.schedulerutmiit.core.common.result.Result
 import com.egormelnikoff.schedulerutmiit.core.network.api.GithubApi
 import com.egormelnikoff.schedulerutmiit.core.network.helper.NetworkExecutor
+import com.egormelnikoff.schedulerutmiit.core.network.mapper.toDomain
 import com.egormelnikoff.schedulerutmiit.latest_release.domain.repos.LatestReleaseDataSource
 import javax.inject.Inject
 
@@ -11,7 +12,14 @@ class LatestReleaseDataSourceImpl @Inject constructor(
     private val githubApi: GithubApi,
     private val networkExecutor: NetworkExecutor
 ) : LatestReleaseDataSource {
-    override suspend fun fetchLatestRelease(): Result<LatestRelease> = networkExecutor.callApi {
-        githubApi.getLatestRelease()
+    override suspend fun fetchLatestRelease(): Result<LatestRelease> {
+        val result = networkExecutor.callApi {
+            githubApi.getLatestRelease()
+        }
+
+        return when (result) {
+            is Result.Error -> result
+            is Result.Success -> Result.Success(result.data.toDomain())
+        }
     }
 }
