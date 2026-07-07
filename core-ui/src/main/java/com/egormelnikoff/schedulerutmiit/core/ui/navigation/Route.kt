afaa3a2403410391cedf09f307771f306510ace9
egor <goodperson2033@gmail.com>
2026-07-07 20:47:04 +0300

    Optimized routing

--- core-ui/src/main/java/com/egormelnikoff/schedulerutmiit/core/ui/navigation/Route.kt
+++ core-ui/src/main/java/com/egormelnikoff/schedulerutmiit/core/ui/navigation/Route.kt
@@ -1,12 +1,6 @@
 package com.egormelnikoff.schedulerutmiit.core.ui.navigation
 
 import androidx.navigation3.runtime.NavKey
-import com.egormelnikoff.schedulerutmiit.core.common.domain.Event
-import com.egormelnikoff.schedulerutmiit.core.common.domain.EventExtraData
-import com.egormelnikoff.schedulerutmiit.core.common.domain.Recurrence
-import com.egormelnikoff.schedulerutmiit.core.common.domain.Schedule
-import com.egormelnikoff.schedulerutmiit.core.common.domain.Task
-import com.egormelnikoff.schedulerutmiit.core.common.enums.TimetableType
 import kotlinx.serialization.Serializable
 import java.time.LocalDate
 
@@ -27,38 +21,26 @@ sealed class Route : NavKey {
         data object NewsList : Dialog()
 
         data class EventDialog(
-            val namedScheduleId: Long,
-            val schedule: Schedule,
-            val isSavedSchedule: Boolean,
-            val event: Event,
-            val date: LocalDate,
-            val eventExtraData: EventExtraData?
+            val eventId: Long,
+            val date: LocalDate
         ) : Dialog()
-
-        data object AddTaskDialog : Dialog()
 
         data class EditTaskDialog (
-            val task: Task
+            val taskId: Long,
+            val date: LocalDate
         ) : Dialog()
 
-        data class AddEditEventDialog(
-            val namedScheduleId: Long,
-            val scheduleId: Long,
-            val recurrence: Recurrence?,
-            val scheduleStartDate: LocalDate,
-            val scheduleEndDate: LocalDate,
-            val updatableEvent: Event? = null
+        data class EditEventDialog(
+            val eventId: Long?,
+            val scheduleId: Long
         ) : Dialog()
 
         data class HiddenEventsDialog(
-            val namedScheduleId: Long,
-            val namedScheduleShortName: String,
-            val timetableType: TimetableType?
+            val scheduleId: Long
         ) : Dialog()
 
         data class RenameNamedScheduleDialog(
-            val namedScheduleId: Long,
-            val namedScheduleFullName: String
+            val namedScheduleId: Long
         ) : Dialog()
 
         data class NewsDialog(
@@ -68,5 +50,6 @@ sealed class Route : NavKey {
         data object SearchDialog : Dialog()
         data object CurriculumDialog : Dialog()
         data object AddScheduleDialog : Dialog()
+        data object AddTaskDialog : Dialog()
     }
 }
