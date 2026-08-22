bef288fe95d8ef4f32a0925a58e13b2a139549f6
egor <goodperson2033@gmail.com>
2026-08-22 17:57:30 +0300

    Added imePadding

--- feature-schedule/src/main/java/com/egormelnikoff/schedulerutmiit/schedule/ui/dialog/event/Event.kt
+++ feature-schedule/src/main/java/com/egormelnikoff/schedulerutmiit/schedule/ui/dialog/event/Event.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -101,6 +102,7 @@ fun EventDialog(
         is EventState.Loading -> LoadingScreen()
         is EventState.Loaded -> {
             Scaffold(
+                modifier = Modifier.imePadding(),
                 topBar = {
                     CustomTopAppBar(
                         navAction = onBack,
